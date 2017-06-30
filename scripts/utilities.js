a8f3f68445c0f7309337c3dfc506e36fe34a9c38
Superman296 <minhmystery@gmail.com>
2017-06-29 22:17:46 -0700

    Checkpoint 9 Assignment completed

---
+++ scripts/utilities.js
@@ -0,0 +1,8 @@
+var forEach = function(array, callback) {
+    for (var i = 0; i < array.length; i++){
+        callback(array[i]);
+    }
+};
+
+//Use a loop to go through all elements in the points array.
+//Execute a callback for each element.
