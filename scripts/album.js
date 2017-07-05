f42e43dada1aad6ef5fdbf98247697cdc8b0dd11
Superman296 <minhmystery@gmail.com>
2017-07-04 23:08:06 -0700

    Checkpoint 13 Completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,4 +1,3 @@
-// Example Album
  var albumPicasso = {
      title: 'The Colors',
      artist: 'Pablo Picasso',
@@ -13,8 +12,7 @@
          { title: 'Magenta', duration: '2:15'}
      ]
  };
- 
- // Another Example Album
+
  var albumMarconi = {
      title: 'The Telephone',
      artist: 'Guglielmo Marconi',
@@ -30,13 +28,12 @@
      ]
  };
 
-//3rd album
 var albumMade = {
      title: 'Made',
      artist: 'Big Bang',
      label: 'YG',
      year: '2016',
-     albumArtUrl: 'assets/images/album_covers/M_cover_Bigbang.jpg',
+     albumArtUrl:'assets/images/album_covers/M_cover_Bigbang.jpg',
      songs: [
          { title: 'Bang Bang Bang', duration: '3:40' },
          { title: 'FXXK It', duration: '3:51' },
@@ -54,64 +51,123 @@ var createSongRow = function(songNumber, songName, songLength) {
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
       ;
- 
+
      return template;
  };
 
 
 var setCurrentAlbum = function(album) {
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-
-     // #2
+    
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
- 
-     // #3
+
      albumSongList.innerHTML = '';
- 
-     // #4
+
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
-  var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 
- window.onload = function() {
-     setCurrentAlbum(albumPicasso);
-     var albums = ["albumPicasso", "albumMarconi", "albumMade"];
-     var index = 0;
-     albumImage.addEventListener("click", function(event){
-         setCurrentAlbum(albums[index]);
-         current++;
-         if (index == albums.length){
-             index = 0;
-         }});
- };
 
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+
+var findParentByClassName = function(element, targetClass) {
+    if (element) {
+        var currentParent = element.parentElement;
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+            if (currentParent === null){
+                console.log("No parent found");
+                console.log("No parent found with that class name");
+            }
+        }
+        return currentParent;
+    }
+}
+
+var getSongItem = function(element){
+    switch(element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
+
+var clickHandler = function(targetElement) {
+    var songItem = getSongItem(targetElement);
+    if (currentlyPlayingSong === null) {
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+         songItem.innerHTML = playButtonTemplate;
+         currentlyPlayingSong = null;
+     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     }
+};
+
 var songRows = document.getElementsByClassName('album-view-song-item');
 
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
- window.onload = function() {
-     setCurrentAlbum(albumPicasso);
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
-     songListContainer.addEventListener('mouseover', function(event) {
-         // #1
-         if (event.target.parentElement.className === 'album-view-song-item') {
- event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-         }
-     });
+ var currentlyPlayingSong = null;
+
+
+ window.onload = function() {
+    setCurrentAlbum(albumPicasso);
+    var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
      
-      for (var i = 0; i < songRows.length; i++) {
-         songRows[i].addEventListener('mouseleave', function(event) {
- this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    songListContainer.addEventListener('mouseover', function(event) 
+    {  
+        if (event.target.parentElement.className === 'album-view-song-item') {
+                var songItem = getSongItem(event.target);
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+                    }
+            }
+    });
+     
+          for (var i = 0; i < songRows.length; i++) {
+             songRows[i].addEventListener('mouseleave', function(event) {
+                 var songItem = getSongItem(event.target);
+             var songItemNumber = songItem.getAttribute('data-song-number');
+             if (songItemNumber !== currentlyPlayingSong) {
+                 songItem.innerHTML = songItemNumber;   
+             }
+      });
+            songRows[i].addEventListener('click', function(event) {
+            clickHandler(event.target);
          });
      }
- };
-    
-        
+ }
+
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     var albums = [albumPicasso, albumMarconi, albumMade];
+     var index = 1;
+     albumImage.addEventListener("click", function(event){
+         setCurrentAlbum(albums[index]);
+         index++;
+         if (index == albums.length){
+             index = 0;
+         }
+     });
+ 
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,25 +1,26 @@
-var pointsArray = document.getElementsByClassName('point');
+//wdt-students sdt-students wdt-standup geekbot 
 
- var revealPoint = function(index){
-    points.style.opacity = 1;
-         points.style.transform = "scaleX(1) translateY(0)";
-         points.style.msTransform = "scaleX(1) translateY(0)";
-         points.style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
+var revealPoint = function(point){
+         point.style.opacity = 1;
+         point.style.transform = "scaleX(1) translateY(0)";
+         point.style.msTransform = "scaleX(1) translateY(0)";
+         point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
- var animatePoints = function(points) {
+var animatePoints = function(points) {
      forEach(points, revealPoint);
 };
 
 window.onload = function() {
-         var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var pointsArray = document.getElementsByClassName('point');
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
      var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-window.addEventListener('scroll', function(event) {
+    window.addEventListener('scroll', function(event) {
          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
              animatePoints(pointsArray);   
          }
-     });
+    });
 };
 
 //Replace the for loop in the animatePoints function with a forEach block and confirm that the selling points still animate properly.
